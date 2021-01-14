1575b07ef11264790e5c9c141aec52238083330f
Nathan Bell <nathan.bell421@icloud.com>
2021-01-14 16:23:21 -0600

    finisd

--- src/components/PersonCard.js
+++ src/components/PersonCard.js
@@ -2,13 +2,23 @@ import { render } from '@testing-library/react';
 import React, {Component} from 'react';
 
 class PersonCard extends Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            age: this.props.age,
+        };
+    }
+    addYear = () => {
+        this.setState({ age: this.state.age + 1})
+    }
     render(){
-        const {firstName, lastName, age, hairColor} = this.props;
+        const {firstName, lastName, hairColor} = this.props;
         return(
             <div>
                 <h1>{lastName}, {firstName}</h1>
-                <p>Age: {age}</p>
+                <p>Age: {this.state.age}</p>
                 <p>Hair Color: {hairColor}</p>
+                <button onClick={ this.addYear }>Birthday Button</button>
             </div>
         );
     }
